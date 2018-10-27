cca468b8eda3689809155f2f4e5b14a612b68e10
Eric Pereira <ericmp@gmail.com>
2018-10-27 20:05:29 -0300

    Add console.info

--- console.js
+++ console.js
@@ -63,3 +63,6 @@ console.log('%c Dude! this looks awesome till now', 'color: red; font-size: 40px
 
 // View head sizes; This is a property and not a function!
 console.memory
+
+// Prints an informational message, showing a small "i" at the message
+console.info('Foo: %o', foo);
